8907e1f45d7d2aa93e1fb5cb6790ede3fd475eae
Ranjeet Negi <rnjetnegi@users.noreply.github.com>
2015-08-18 17:14:44 +0530

    adding integer variable a

--- Demo1/src/mainpack/MainApp.java
+++ Demo1/src/mainpack/MainApp.java
@@ -3,6 +3,7 @@ package mainpack;
 public class MainApp {
 
 	public static void main(String[] args) {
+		int a;
 		// TODO Auto-generated method stub
                System.out.println("Hello world");
 
